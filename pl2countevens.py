d52e2ff5fdb5e57fee375b24e86ecc3094a8f88f
bitterbooher <qbooher@gmail.com>
2013-07-14 11:58:47 -0600

    finished pl2countevens.py

---
+++ pl2countevens.py
@@ -0,0 +1,20 @@
+"""
+Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1. 
+
+count_evens([2, 1, 2, 3, 4]) -> 3
+count_evens([2, 2, 0]) -> 3
+count_evens([1, 3, 5]) -> 0
+"""
+
+def count_evens(nums):
+	numevens = 0
+	for i in range(len(nums)): # lenreturns length of list
+		if (nums[i] % 2) == 0:
+				numevens += 1
+	print numevens
+
+count_evens([2,1,2,3,4]) # -> 3
+count_evens([2]) # -> 1
+count_evens([2,2]) # -> 2
+count_evens([1]) # -> 0
+			
